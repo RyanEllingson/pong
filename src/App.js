560b10d62de0bea15a68cf0dd7985e6eb9d24df0
Geddy Delonge <57271262+RyanEllingson@users.noreply.github.com>
2020-01-27 13:42:42 -0600

    added obstacles, updated collision physics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useReducer } from "react";
 import "./App.css";
 import Paddle from "./components/Paddle";
 import Ball from "./components/Ball";
+import Brick from "./components/Brick";
 
 
 const initialState = {
@@ -22,7 +23,18 @@ const initialState = {
     y: 0,
     dx: 5,
     dy: 5
-  }
+  },
+  bricks: [
+    {
+      top: 0, left: 300
+    },
+    { 
+      top: 200, left: 400
+    },
+    {
+      top: 390, left: 550
+    }
+  ]
 };
 
 
@@ -44,7 +56,6 @@ function reducer (state, action) {
 }
 
 export default function App() {
-
 
   const [state, dispatch] = useReducer(reducer, initialState); 
 
@@ -79,17 +90,58 @@ export default function App() {
   }
 
   useEffect(() => {
-    window.addEventListener("keydown", handleKeyP1);
+    window.addEventListener("keypress", handleKeyP1);
     window.addEventListener("keyup", releaseKeyP1);
-    window.addEventListener("keydown", handleKeyP2);
+    window.addEventListener("keypress", handleKeyP2);
     window.addEventListener("keyup", releaseKeyP2);
     return () => {
-      window.removeEventListener("keydown", handleKeyP1);
+      window.removeEventListener("keypress", handleKeyP1);
       window.removeEventListener("keyup", releaseKeyP1);
-      window.removeEventListener("keydown", handleKeyP2);
+      window.removeEventListener("keypress", handleKeyP2);
       window.removeEventListener("keyup", releaseKeyP2);
     }
   }, [state]);
+
+  function willCollide(rect1, rect2) {
+    let x = false;
+    let y = false;
+    let xCurr = false;
+    let yCurr = false;
+    let collided = false;
+
+    const rect1XNext = rect1.x + rect1.dx;
+    const rect1YNext = rect1.y + rect1.dy;
+
+    if (rect1.x < rect2.x + rect2.width && rect1.x + rect1.width > rect2.x) {
+      xCurr = true;
+    }
+    if (rect1.y < rect2.y + rect2.height && rect1.y + rect1.height > rect2.y) {
+      yCurr = true;
+    }
+    if (
+      yCurr &&
+      rect1XNext < rect2.x + rect2.width &&
+      rect1XNext + rect1.width > rect2.x
+    ) {
+      x = true;
+    }
+    if (
+      xCurr &&
+      rect1YNext < rect2.y + rect2.height &&
+      rect1YNext + rect1.height > rect2.y
+    ) {
+      y = true;
+    }
+    if (
+      rect1XNext < rect2.x + rect2.width &&
+      rect1XNext + rect1.width > rect2.x &&
+      rect1YNext < rect2.y + rect2.height &&
+      rect1YNext + rect1.height > rect2.y
+    ) {
+      collided = true;
+    }
+    return { x, y, collided };
+  }
 
   useEffect(() => {
     const myTimeout = setTimeout(() => {
@@ -101,31 +153,109 @@ export default function App() {
       let paddle1Y = state.paddle1y.y;
       let paddle2Y = state.paddle2y.y;
 
-      if (x < 5 || x > 700) {
-        return dispatch({
-          type: "MOVE_BALL",
-          payload: {
-            dx: 5,
-            dy: 5,
-            x: 200,
-            y: 0
-          }
-        });
-      }
+      const ball = {
+        x,
+        dx,
+        y,
+        dy,
+        width: 20,
+        height: 20
+      };
 
-      if (x + dx > 750 - 20 || x + dx < 0) {
+      const walls = [
+        // left
+        {
+          x: -100,
+          y: 0,
+          width: 100,
+          height: 500
+        },
+        // right
+        {
+          x: 750,
+          y: 0,
+          width: 100,
+          height: 500
+        },
+        // top
+        {
+          x: 0,
+          y: -100,
+          width: 750,
+          height: 100
+        },
+        // bottom
+        {
+          x: 0,
+          y: 500,
+          width: 750,
+          height: 100
+        }
+      ];
+
+      // if (x < 5 || x > 700) {
+      //   return dispatch({
+      //     type: "MOVE_BALL",
+      //     payload: {
+      //       dx: 5,
+      //       dy: 5,
+      //       x: 200,
+      //       y: 0
+      //     }
+      //   });
+      // }
+
+      // if (x + dx > 750 - 20 || x + dx < 0) {
+      //   dx = -dx;
+      // }
+      // if (y + dy > 500 - 20 || y + dy < 0) {
+      //   dy = -dy;
+      // }
+
+      // if ((paddle1Y < y+dy && paddle1Y + 100 > y+dy) && x < 45) {
+      //   dx = -dx+2;
+      // }
+
+      // if ((paddle2Y < y+dy && paddle2Y + 100 > y+dy) && x > 685) {
+      //   dx = -(dx+2);
+      // }
+      
+      const wallCollisions = walls.map(wall => {
+        return willCollide(ball, wall);
+      });
+
+      if (wallCollisions[0].collided || wallCollisions[1].collided) {
         dx = -dx;
       }
-      if (y + dy > 500 - 20 || y + dy < 0) {
+
+      if (wallCollisions[2].collided || wallCollisions[3].collided) {
         dy = -dy;
       }
 
-      if ((paddle1Y < y+dy && paddle1Y + 100 > y+dy) && x < 45) {
-        dx = -dx+2;
-      }
+      const obstacleCols = [
+        {
+          left: 20,
+          top: paddle1Y
+        },
+        {
+          left: 705,
+          top: paddle2Y
+        },
+        ...state.bricks
+      ].map(ob => {
+        return willCollide(ball, {
+          width: 25,
+          height: 100,
+          x: ob.left,
+          y: ob.top
+        });
+      });
 
-      if ((paddle2Y < y+dy && paddle2Y + 100 > y+dy) && x > 685) {
-        dx = -(dx+2);
+      if (obstacleCols.some(obc => obc.y)) {
+        dy = -dy;
+      }
+      if (obstacleCols.some(obc => obc.x)) {
+        dx = -dx;
       }
 
       dispatch({
@@ -170,8 +300,13 @@ export default function App() {
     return () => clearTimeout(myTimeout);
   }, [state]);
 
+  const brickList = state.bricks.map((brickObj) => 
+    <Brick style={{top: brickObj.top, left: brickObj.left}}/>
+  )
+
   return (
     <div className="container">
+      {brickList}
       <Paddle paddleY={state.paddle1y.y}/>
       <Paddle isPlayerTwo paddleY={state.paddle2y.y}/>
       <Ball pos={state.ball}/>
